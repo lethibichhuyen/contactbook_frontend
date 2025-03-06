e1652aef2e3d8388f315b0c8d0b441a40f819fdc
LE THI BICH HUYEN <huyenb2106836@student.ctu.edu.vn>
2025-03-06 14:09:29 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -33,6 +33,16 @@
           <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact="activeContact" />
+        <router-link
+          :to="{
+            name: 'contact.edit',
+            params: { id: activeContact._id },
+          }"
+        >
+          <span class="mt-2 badge badge-warning">
+            <i class="fas fa-edit"></i> Hiệu chỉnh</span
+          >
+        </router-link>
       </div>
     </div>
   </div>
@@ -121,5 +131,4 @@ export default {
   text-align: left;
   max-width: 750px;
 }
-
 </style>
